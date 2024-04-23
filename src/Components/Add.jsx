b1a57bffa6db54994f2bf6d574d6fd986f38fa58
akshayvsanthosh <akshaysanthosh12345@gmail.com>
2024-04-23 11:03:33 +0530

    media player updated

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -2,10 +2,28 @@ import React, { useState } from 'react'
 import {Modal, Button, FloatingLabel, Form} from 'react-bootstrap';
 
 function Add() {
+    const [invalidYouTubeUrl,setInvalidYouTubeUrl] = useState(false)
+    const [videoDetails,setVideoDetails] = useState({
+      caption:"",imgUrl:"",youTubeUrl:""
+    })
     const [show, setShow] = useState(false);
 
+    console.log(videoDetails);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const getEmbedURL = (link) => {
+      // https://www.youtube.com/watch?v=L0yEMl8PXnwc
+      // https://www.youtube.com/embed/L0yEMl8PXnw
+        if (link.includes("v=")) {
+          let videoId = link.split("v=")[1].slice(0,11)
+          console.log(videoId);
+          setVideoDetails({...videoDetails,youTubeUrl:`https://www.youtube.com/embed/${videoId}`})
+          setInvalidYouTubeUrl(false)
+        }else{
+          setVideoDetails({...videoDetails,youTubeUrl:""})
+          setInvalidYouTubeUrl(true)
+        }
+    }
 
   return (
     <>
@@ -21,14 +39,17 @@ function Add() {
         <Modal.Body>
           <p>Please Fill the following details!!!</p>
           <FloatingLabel controlId="floatingInput" label="Video Caption" className="mb-3">
-            <Form.Control type="email" placeholder="Video Caption" />
+            <Form.Control onChange={e=>setVideoDetails({...videoDetails,caption:e.target.value})} type="text" placeholder="Video Caption" />
           </FloatingLabel>
           <FloatingLabel controlId="floatingInput" label="Image URL" className="mb-3">
-            <Form.Control type="email" placeholder="Image URL" />
+            <Form.Control onChange={e=>setVideoDetails({...videoDetails,imgUrl:e.target.value})} type="text" placeholder="Image URL" />
           </FloatingLabel>
           <FloatingLabel controlId="floatingInput" label="YouTube URL" className="mb-3">
-            <Form.Control type="email" placeholder="YouTube URL" />
+            <Form.Control onChange={e=>getEmbedURL(e.target.value)}  type="text" placeholder="YouTube URL" />
           </FloatingLabel>
+          {
+            invalidYouTubeUrl && <div className='text-danger fw-bolder'>Invalid youtube url</div>
+          }
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
